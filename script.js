f7ffb646fdd09a409a3f6e924ffef10e126594ea
kingayash2790 <99525427+kingayash2790@users.noreply.github.com>
2024-02-17 18:22:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  });
+}
+
+// Close Navbar Menu on Click Menu Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-active");
+    navbarMenu.classList.remove("is-active");
+  });
+});
+
+// Change Header Background on Scrolling
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 85) {
+    headerMenu.classList.add("on-scroll");
+  } else {
+    headerMenu.classList.remove("on-scroll");
+  }
+});
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    if (navbarMenu.classList.contains("is-active")) {
+      navbarMenu.classList.remove("is-active");
+    }
+  }
+});
+
+// Countdown timer
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var tenMinutes = 60 * 10,
+    display = document.getElementById("timer");
+  startTimer(tenMinutes, display);
+};
+
+// JavaScript for Slide-up Form
+setTimeout(function () {
+  document.getElementById("slideup-form").style.bottom = "0";
+}, 15000);
